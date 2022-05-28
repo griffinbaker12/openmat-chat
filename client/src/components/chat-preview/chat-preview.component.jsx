fffcc449b0abd45c03c59e47a73a3d35e7ffc782
griffinbaker12 <griffin_baker12@icloud.com>
2022-05-28 00:00:16 -0500

    improved conversations context

--- client/src/components/chat-preview/chat-preview.component.jsx
+++ client/src/components/chat-preview/chat-preview.component.jsx
@@ -4,11 +4,12 @@ import './chat-preview.styles.scss';
 
 const ChatPreview = ({ sideBarCategory }) => {
   // And then we can also pull the active conversation up into higher state or into a context just so that we can actually store this variable without losing it when we switch between categories b/c that triggers a re-render
-  const { conversations } = useConversations();
-  const [activePreview, setActivePreview] = useState('');
-  console.log('re-runs', activePreview);
+  const { conversations, activeConversation, setConversation } =
+    useConversations();
 
-  const handleClick = e => setActivePreview(e.target.getAttribute('name'));
+  console.log(conversations, activeConversation);
+
+  const handleClick = e => setConversation(e.target.getAttribute('name'));
 
   // Need to create 2 components, one for the friends view and one for the convo preview view
   // Also going to need a map down below where I map over all of the data that I am pulling in and then add the class to the right; could also do a name function as well where you get the name off of the element that was clicked and then set it to the active one
@@ -22,7 +23,7 @@ const ChatPreview = ({ sideBarCategory }) => {
             key={id}
             name={`conversation-${id}`}
             className={`chat-preview-list ${
-              activePreview === `conversation-${id}` ? 'active' : ''
+              activeConversation === `conversation-${id}` ? 'active' : ''
             }`}
           >
             {conversation.recipients
--- client/src/contexts/conversations-context.js
+++ client/src/contexts/conversations-context.js
@@ -6,13 +6,22 @@ export const useConversations = () => useContext(ConversationsContext);
 
 export const ConversationsProvider = ({ children }) => {
   const [conversations, setConversations] = useState([]);
+  const [activeConversation, setActiveConversation] = useState('');
 
   const createConversation = recipients =>
     setConversations(prevState => [...prevState, { recipients, messages: [] }]);
 
+  const setConversation = conversationAttribute =>
+    setActiveConversation(conversationAttribute);
+
   return (
     <ConversationsContext.Provider
-      value={{ conversations, createConversation }}
+      value={{
+        conversations,
+        createConversation,
+        activeConversation,
+        setConversation,
+      }}
     >
       {children}
     </ConversationsContext.Provider>
