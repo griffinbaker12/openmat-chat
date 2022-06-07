757c58b95f7780ed45abca7630adc1ede59442f4
griffinbaker12 <griffin_baker12@icloud.com>
2022-06-07 18:15:46 -0400

    updated add friend modal

--- client/src/components/chat-participant/chat-participant.component.jsx
+++ client/src/components/chat-participant/chat-participant.component.jsx
@@ -1,7 +1,14 @@
 import './chat-participant.styles.scss';
 
 const ChatParticipant = ({ chatParticipant }) => {
-  return <div className="chat-participant-content">{chatParticipant}</div>;
+  return (
+    <div className="chat-participant-content-container">
+      <div className="chat-participant-content-participant">
+        {chatParticipant}
+      </div>
+      <div className="chat-participant-content-remove">&#x2715;</div>
+    </div>
+  );
 };
 
 export default ChatParticipant;
--- client/src/components/new-conversation-modal/new-conversation-modal.component.jsx
+++ client/src/components/new-conversation-modal/new-conversation-modal.component.jsx
@@ -3,6 +3,7 @@ import ChatParticipant from '../chat-participant/chat-participant.component';
 import { useConversations } from '../../contexts/conversations-context';
 import './new-conversation-modal.styles.scss';
 import { useSidebar } from '../../contexts/sidebar-context';
+import { useAuthentication } from '../../contexts/authentication-context';
 
 // There seems to also totally be room to just have one modal component, and also a genreal button component as well, but for the modal, essentially I can just pass in the name of the modal itself, and then the body of the modal. The 'children' can just be the unique part of the actual form for these components that gets inserted into the overall block of the component
 
@@ -12,8 +13,12 @@ const NewConversationModal = () => {
   const { closeModal } = useSidebar();
   const { createConversation } = useConversations();
   const [chatParticipants, setChatParticipants] = useState([]);
-  const userNameRef = useRef();
-  // const nameRef = useRef();
+  const [formInput, setFormInput] = useState({ chatName: '', name: '' });
+  const [isLoading, setIsLoading] = useState(false);
+  const { currentUser } = useAuthentication();
+  const [searchResults, setSearchResults] = useState([]);
+
+  // So keep track of all the participants here, will need to search as well, then when we make the chat we send this to the api and push this into the current chat state of our application, also need to check to see if a chat with these users has already been made...
 
   // This is fine for now, I guess at some point I need to make it clear whether or not you can submit this when you hit enter, or whether hitting enter adds people to the chat if there are none, but then it probably should stary like that as well even afterwards so may make the other button a button type as well
 
@@ -25,33 +30,76 @@ const NewConversationModal = () => {
 
   const handleChatCreation = () => {
     if (chatParticipants.length === 0) return;
-    createConversation(chatParticipants);
-    closeModal();
-    userNameRef.current.value = '';
-    setChatParticipants([]);
+
+    // createConversation(chatParticipants);
+    // closeModal();
+    // setFormInput({ chatName: '', name: '' });
+    // setChatParticipants([]);
+  };
+
+  const handleChange = async e => {
+    const field = e.target.getAttribute('name');
+    setFormInput(prevState => ({ ...prevState, [field]: e.target.value }));
+
+    // If we are typing into the name field, then we want to make a fetch reqeust with the current value of the input
+    if (field !== 'name') return;
+    const query = formInput.name;
+
+    try {
+      fetch('');
+      setIsLoading(true);
+      const response = await fetch(
+        `http://localhost:4000/api/user?search=${query}`,
+        {
+          method: 'get',
+          headers: { Authorization: `Bearer ${currentUser.token}` },
+        }
+      );
+      const { users } = await response.json();
+      console.log(users);
+      setIsLoading(false);
+      setSearchResults(users);
+    } catch (e) {}
   };
 
   const handleAddUser = () => {
-    const user = userNameRef.current.value;
-    if (!user) return;
+    // const user = userNameRef.current.value;
+    if (!formInput.name) return;
     // Will need to do some check to see if the actual user exists in the database or not
     // If they do exist...
     // This user may also at some point may also have some data on them besides just the userName, but suffices for now
-    setChatParticipants(prevState => [...prevState, user]);
-    userNameRef.current.value = '';
+    setChatParticipants(prevState => [...prevState, formInput.name]);
+    setFormInput(prevState => ({ ...prevState, name: '' }));
   };
 
   return (
     <div className="new-conversation-modal-body">
       <form onSubmit={handleSubmit}>
-        <label>Username</label>
+        <label>Chat Name</label>
         <input
-          ref={userNameRef}
+          onChange={handleChange}
+          type="text"
+          required={true}
+          name="chatName"
+          value={formInput.chatName}
+        />
+        <label>Name</label>
+        <input
+          onChange={handleChange}
           type="text"
           required={chatParticipants.length > 0 ? false : true}
+          name="name"
+          value={formInput.name}
         />
+        {/* So in here we need the searched users as well as the selected users */}
         <div className="new-conversation-modal-chat-participant-container">
           {chatParticipants.length !== 0 &&
+            chatParticipants.map((chatParticipant, i) => (
+              <ChatParticipant key={i} chatParticipant={chatParticipant} />
+            ))}
+        </div>
+        <div className="new-conversation-modal-chat-search-result-container">
+          {isLoading ? searchResults.length !== 0 &&
             chatParticipants.map((chatParticipant, i) => (
               <ChatParticipant key={i} chatParticipant={chatParticipant} />
             ))}
